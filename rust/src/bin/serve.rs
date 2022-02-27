709de7fb461d2c523d7fc774831f21cfc68358de
Brice Fernandes <brice@fractallambda.com>
2022-02-27 13:50:16 +0000

    Enable multistage build with libs enabled

--- rust/src/bin/serve.rs
+++ rust/src/bin/serve.rs
@@ -1,10 +1,12 @@
 use clap::{Arg, Command};
-
-
 use tide::Request;
 use tide::prelude::*;
+use prometheus::{
+    HistogramOpts, HistogramVec, IntCounter, IntCounterVec, IntGauge, Opts, Registry,  /*Counter, TextEncoder, Encoder*/
+};
+use lazy_static::lazy_static;
 
-
+use testaroo::metrics::hello;
 
 #[derive(Debug, Deserialize)]
 struct Animal {
@@ -12,22 +14,57 @@ struct Animal {
     legs: u8,
 }
 
-
+// Application server Options
 struct ServerOptions {
     port: u16
 }
 
+// Metric server options
+// struct MetricsOptions {
+//     port: u16
+// }
+
+
 async fn start_server(options: ServerOptions) -> tide::Result<()>{
     println!("Starting server");
-    let mut app = tide::new();
-    app.at("/orders/shoes").post(order_shoes);
-    app.listen(format!("127.0.0.1:{}", options.port)).await?;
+    let mut server = tide::new();
+    server.at("/orders/shoes").post(order_shoes);
+    server.listen(format!("127.0.0.1:{}", options.port)).await?;
     Ok(())
 }
 
 
+lazy_static! {
+    pub static ref REGISTRY: Registry = Registry::new();
+
+    pub static ref INCOMING_REQUESTS: IntCounter =
+        IntCounter::new("incoming_requests", "Incoming Requests").expect("metric can be created");
+
+    pub static ref CONNECTED_CLIENTS: IntGauge =
+        IntGauge::new("connected_clients", "Connected Clients").expect("metric can be created");
+
+    pub static ref RESPONSE_CODE_COLLECTOR: IntCounterVec = IntCounterVec::new(
+        Opts::new("response_code", "Response Codes"),
+        &["env", "statuscode", "type"]
+    )
+    .expect("metric can be created");
+
+    pub static ref RESPONSE_TIME_COLLECTOR: HistogramVec = HistogramVec::new(
+        HistogramOpts::new("response_time", "Response Times"),
+        &["env"]
+    )
+    .expect("metric can be created");
+}
+
+// fn metrics() {
+//     let encoder = TextEncoder::new();
+
+// }
+
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
+    println!("{}", hello());
 
     let matches = Command::new("My Test Program")
         .version("0.1.0")
