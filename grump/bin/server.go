f5772af814834f7cd96d929f03b61948188203f8
Brice Fernandes <brice.fernandes@raspberrypi.org>
2024-10-17 18:13:31 +0100

    Update with mustache rendering

--- grump/bin/server.go
+++ grump/bin/server.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"io"
 	"net/http"
+	"time"
 
-	"github.com/a-h/templ"
-	"github.com/bricef/grump/templates"
-	"github.com/cbroglie/mustache"
+	"github.com/bricef/grump/internal/rendering"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 )
 
 type LoginPage struct {
@@ -16,61 +16,53 @@ type LoginPage struct {
 	Password string // {{ .Password }}
 }
 
-type MustacheRenderer map[string]string
-
-func (mr MustacheRenderer) Render(w io.Writer, name string, data interface{}, ctx echo.Context) error {
-	template, err := mustache.Render(mr[name], data)
-	if err != nil {
-		return err
-	}
-	_, err = w.Write([]byte(template))
-	return err
+type ErrorInfo struct {
+	Code    int
+	Message string
 }
 
-func render(ctx echo.Context, status int, t templ.Component) error {
-	ctx.Response().Writer.WriteHeader(status)
-
-	err := t.Render(ctx.Request().Context(), ctx.Response().Writer)
-	if err != nil {
-		return ctx.String(http.StatusInternalServerError, "failed to render response template")
-	}
-
-	return nil
+type DateInfo struct {
+	Date time.Time
 }
 
-func customHTTPErrorHandler(err error, ctx echo.Context) {
-	code := http.StatusInternalServerError
-	if he, ok := err.(*echo.HTTPError); ok {
-		code = he.Code
-	}
-	render(ctx, code, templates.ErrorPage(code))
-}
+var CACHE_TEMPLATE = false
 
 func main() {
 
 	e := echo.New()
-	e.Renderer = MustacheRenderer{
-		"a": "I am template A {{data}}",
-		"b": "I am template B {{data}}",
+
+	e.Logger.SetLevel(log.DEBUG)
+
+	e.Renderer = rendering.NewMustacheRenderer(rendering.MustacheRendererConfig{
+		caching:           false,
+		root_directory:    "./templates/",
+		layouts_directory: "layouts/",
+	})
+
+	e.HTTPErrorHandler = func(err error, ctx echo.Context) {
+		code := http.StatusInternalServerError
+
+		if he, ok := err.(*echo.HTTPError); ok {
+			code = he.Code
+			ctx.Render(code, "pages/error", he)
+		} else {
+			ctx.Render(code, "pages/error", ErrorInfo{Code: http.StatusInternalServerError, Message: "Server encountered an internal error"})
+		}
 	}
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.GET("/mustache", func(c echo.Context) error {
-		data, err := mustache.Render("hello {{c}}", map[string]string{"c": "from mustache"})
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, "failed to render template")
-		}
-		return c.String(http.StatusOK, data)
+		data := map[string]string{"c": "from mustache"}
+		return c.Render(http.StatusOK, "pages/mustache", data)
 	})
-	e.GET("/a", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "a", map[string]string{"data": "from a"})
+	e.GET("/simple", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "pages/simple", nil)
 	})
-	e.GET("/b", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "b", map[string]string{"data": "from b"})
+	e.GET("/date", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "partials/date", &DateInfo{time.Now()})
 	})
 
-	e.HTTPErrorHandler = customHTTPErrorHandler
 	e.Logger.Fatal(e.Start(":1323"))
 }
